1bad525c945bf26347d37d4d38c817958b8e5f97
SurajKumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2023-06-02 16:29:09 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,14 +1,16 @@
-/* How data is kept in memory and how it is accessed on that charaterization we have 2 type of data types
-    a.Primitive(Call by value) - String, Number, Boolean, null, undefined, Symbol, BigInt
-    b.Non-Primitive(Reference type). - Array, Objects, Functions
+/* How data is kept in memory and how it is accessed on that  we have 2 type of data types.
+
+    a.Primitive data types (Call by value) - String, Number, Boolean, null, undefined, Symbol, BigInt.(What ever changes you do in it you do it in copy).
+    
+    b.Non-Primitive data types (Reference type) - Array, Objects, Functions.(What ever changes you do in it you do it in original copy).
 */
 
-// Javascript is Dynamically typed language because we dont have to specify variables datatype (int, string, boolean etc).
+// Javascript is Dynamically typed language because we don't have to specify variables datatype (int, string, boolean etc).
 
-const score = "100"; 
+const score = "100";
 console.log(typeof score); // String
 
-const scoreResult = 100; 
+const scoreResult = 100;
 console.log(typeof scoreResult); // Number
 
 const isLoggedIn = false;
@@ -20,22 +22,21 @@ console.log(typeof outsideTemp); // null/object
 let userEmail;
 console.log(typeof userEmail); // Undefined
 
-const id = Symbol('123');
-const anotherId = Symbol('123');
+const id = Symbol("123");
+const anotherId = Symbol("123");
 
 console.log(typeof id); // symbol
-console.log(id == anotherId); // false
+console.log(id === anotherId); // false
 
 const bigNumber = 3456543576654356754n; // bigInt
 console.log(typeof bigNumber);
 
 const heroes = ["shaktiman", "naagraj", "doga"];
 let myObj = {
-    name: "suraj",
-    age: 30,
-}
+  name: "suraj",
+  age: 30,
+};
 
-const myFunction = function(){
-    console.log("Hello World");
-}
-
+const myFunction = function () {
+  console.log("Hello World");
+};
