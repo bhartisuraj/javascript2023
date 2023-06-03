5a81ec7d3d9c069f72218b20d27c8bd280465a2e
SurajKumar Bharti <57192000+bhartisuraj@users.noreply.github.com>
2023-06-03 06:36:32 +0000

    datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,35 +1,35 @@
 /* How data is kept in memory and how it is accessed on that  we have 2 type of data types.
 
-    a.Primitive data types (Call by value) - String, Number, Boolean, null, undefined, Symbol, BigInt.(What ever changes you do in it you do it in copy).
+    a.Primitive data types (Call by value) Stack Memory - String, Number, Boolean, null, undefined, Symbol, BigInt.(What ever changes you do in it you do it in copy).
     
-    b.Non-Primitive data types (Reference type) - Array, Objects, Functions.(What ever changes you do in it you do it in Reference copy).
+    b.Non-Primitive data types (Reference type) Heap Memory - Array, Objects, Functions.(What ever changes you do in it you do it in Reference copy of original value).
 */
 
 // Javascript is Dynamically typed language because we don't have to specify variables datatype (int, string, boolean etc).
 
 const score = "100";
-console.log(typeof score); // String
+// console.log(typeof score); // String
 
 const scoreResult = 100;
-console.log(typeof scoreResult); // Number
+// console.log(typeof scoreResult); // Number
 
 const isLoggedIn = false;
-console.log(typeof isLoggedIn); // Boolean
+// console.log(typeof isLoggedIn); // Boolean
 
 const outsideTemp = null;
-console.log(typeof outsideTemp); // null/object
+// console.log(typeof outsideTemp); // null/object
 
 let userEmail;
-console.log(typeof userEmail); // Undefined
+// console.log(typeof userEmail); // Undefined
 
 const id = Symbol("123");
 const anotherId = Symbol("123");
 
-console.log(typeof id); // symbol
-console.log(id === anotherId); // false
+// console.log(typeof id); // symbol
+// console.log(id === anotherId); // false
 
 const bigNumber = 3456543576654356754n; // bigInt
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 const heroes = ["shaktiman", "naagraj", "doga"];
 let myObj = {
@@ -38,5 +38,30 @@ let myObj = {
 };
 
 const myFunction = function () {
-  console.log("Hello World");
+  // console.log("Hello World");
 };
+
+// +++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myName = "surajbharti.com";
+
+let anotherName = myName;
+
+anotherName = "suraj";
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+  email: "user@google.com",
+  upi: "user@ybl",
+};
+
+let userTwo = userOne;
+
+userTwo.email = "suraj@google.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
